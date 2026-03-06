8fac6cb333dab4fea7e163359fbe4f9a6b1bbeb9
Saalim Quadri <danascape@gmail.com>
2026-03-07 01:32:47 +0530

    SUW: Add Android-Studio support
    Change-Id: I80142cc0bf522857931d7f598c087d919c80d943
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SetupWizard"
+
+include(":app")
