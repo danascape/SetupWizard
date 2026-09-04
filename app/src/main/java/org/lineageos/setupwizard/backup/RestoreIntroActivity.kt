66502f03301cdb0c5b512fb4e2943339843eefab
Saalim Quadri <danascape@gmail.com>
2026-09-04 19:00:30 +0530

    SUW: Swap skip/backup buttons
    Change-Id: I995c3e0cc0987502741c6f8f9b5e586fa0cfa2ec
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- app/src/main/java/org/lineageos/setupwizard/backup/RestoreIntroActivity.kt
+++ app/src/main/java/org/lineageos/setupwizard/backup/RestoreIntroActivity.kt
@@ -10,7 +10,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.ActivityResult
 import com.google.android.setupcompat.util.ResultCodes.RESULT_ACTIVITY_NOT_FOUND
-import com.google.android.setupcompat.util.ResultCodes.RESULT_SKIP
 import org.lineageos.setupwizard.R
 import org.lineageos.setupwizard.SetupWizardApp.Companion.ACTION_RESTORE_FROM_BACKUP
 import org.lineageos.setupwizard.base.SubBaseActivity
@@ -22,8 +21,7 @@ class RestoreIntroActivity : SubBaseActivity() {
         glifLayout.setDescriptionText(
             getString(R.string.intro_restore_subtitle, getString(R.string.os_name))
         )
-        setNextText(R.string.skip)
-        setSkipText(R.string.intro_restore_button)
+        setNextText(R.string.intro_restore_button)
     }
 
     override fun onSubactivityResult(activityResult: ActivityResult) {
@@ -41,10 +39,6 @@ class RestoreIntroActivity : SubBaseActivity() {
     }
 
     override fun onNextPressed() {
-        nextAction(RESULT_SKIP)
-    }
-
-    override fun onSkipPressed() {
         launchRestore()
     }
 
