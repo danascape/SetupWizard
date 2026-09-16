c81c099bf43e0f97e021f51cd66564bd7c387e3b
Saalim Quadri <danascape@gmail.com>
2026-09-16 23:28:35 +0530

    SUW: Convert LocationSettings to GlifRecyclerLayout
    Based on the design provided by Asher Simonds.
    
    Co-authored-by: Asher S <dayanhammer@gmail.com>
    Change-Id: Ib815db46c3400f13a4a9e6a57bddd21861ac4ee9
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- app/src/main/java/org/lineageos/setupwizard/privacy/LocationSettingsActivity.kt
+++ app/src/main/java/org/lineageos/setupwizard/privacy/LocationSettingsActivity.kt
@@ -11,40 +11,39 @@ import android.os.Bundle
 import android.os.Process
 import android.os.UserManager
 import android.provider.Settings
-import android.view.View
-import android.widget.CheckBox
+import com.google.android.setupdesign.GlifRecyclerLayout
+import com.google.android.setupdesign.items.RecyclerItemAdapter
+import com.google.android.setupdesign.items.SwitchItem
 import org.lineageos.setupwizard.R
 import org.lineageos.setupwizard.base.BaseSetupWizardActivity
 
 class LocationSettingsActivity : BaseSetupWizardActivity() {
 
-    private lateinit var locationAccess: CheckBox
-    private lateinit var locationAgpsAccess: CheckBox
+    private val itemAdapter by lazy {
+        (glifLayout as GlifRecyclerLayout).adapter as RecyclerItemAdapter
+    }
 
-    private lateinit var locationManager: LocationManager
-    private lateinit var userManager: UserManager
+    private val locationAccess by lazy {
+        itemAdapter.findItemById(R.id.location_item) as SwitchItem
+    }
+    private val locationAgpsAccess by lazy {
+        itemAdapter.findItemById(R.id.location_agps_item) as SwitchItem
+    }
+
+    private val locationManager by lazy { getSystemService(LocationManager::class.java) }
+    private val userManager by lazy { getSystemService(UserManager::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setNextText(R.string.next)
+        glifLayout.setDescriptionText(getString(R.string.location_summary))
 
-        locationAccess = findViewById(R.id.location_checkbox)
-        locationAgpsAccess = findViewById(R.id.location_agps_checkbox)
-        locationManager = getSystemService(LocationManager::class.java)
-        userManager = getSystemService(UserManager::class.java)
+        locationAgpsAccess.isVisible = userManager.isMainUser
 
-        val locationAccessView = findViewById<View>(R.id.location)
-        locationAccessView.setOnClickListener {
-            locationAccess.isChecked = !locationAccess.isChecked
-        }
-
-        val locationAgpsAccessView = findViewById<View>(R.id.location_agps)
-        if (userManager.isMainUser) {
-            locationAgpsAccessView.setOnClickListener {
-                locationAgpsAccess.isChecked = !locationAgpsAccess.isChecked
+        itemAdapter.setOnItemSelectedListener { item ->
+            if (item is SwitchItem) {
+                item.isChecked = !item.isChecked
             }
-        } else {
-            locationAgpsAccessView.visibility = View.GONE
         }
     }
 
