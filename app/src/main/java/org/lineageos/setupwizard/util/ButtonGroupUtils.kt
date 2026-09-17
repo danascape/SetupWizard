fb913b97352e74bfc8ca963b0d66e550e6ecb39b
Saalim Quadri <danascape@gmail.com>
2026-09-17 11:46:29 +0530

    nav
    Change-Id: I19ac7f620fc5e889d396996ac723ab1749450ad2
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- app/src/main/java/org/lineageos/setupwizard/util/ButtonGroupUtils.kt
+++ app/src/main/java/org/lineageos/setupwizard/util/ButtonGroupUtils.kt
@@ -12,7 +12,7 @@ import com.google.android.material.button.MaterialButtonToggleGroup
 fun MaterialButtonToggleGroup.updateCheckedIcons(@DrawableRes iconResId: Int) {
     for (index in 0 until childCount) {
         val button = getChildAt(index) as? MaterialButton ?: continue
-        if (button.isChecked) {
+        if (button.id == checkedButtonId) {
             button.setIconResource(iconResId)
         } else {
             button.icon = null
